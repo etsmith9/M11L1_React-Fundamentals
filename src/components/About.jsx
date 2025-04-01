8ca49ae75d049adec7615cf86883138083e08a9b
Emily Smith <etsmith9@gmail.com>
2025-04-01 11:33:30 -0400

    Create About.jsx

---
+++ src/components/About.jsx
@@ -0,0 +1,17 @@
+import { Component } from 'react';
+import '../PortfolioStyles.css';
+
+class About extends Component {
+    render() {
+        return (
+            <section>
+                <h2>About Me</h2>
+                <p> Born and raised in the New England portion of the United States, I spent my 20s studying and living in New York City where I was a Project Manager in the Art Logistics field for 10 years.
+I worked with galleries, museums and private collectors on managing their art collections before settling down to start my family.
+Now I am living in New Jersey and studying to become a web developer through Coding Temple's online program.</p>
+            </section>
+        );
+    }
+}
+
+export default About;
